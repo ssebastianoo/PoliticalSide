41b7326122dc2294b0f15b9d8a4e6eb5122a2698
ssebastianoo <sebastianogirotto05@gmail.com>
2022-08-22 16:55:20 +0200

    dashboard now requires login

--- src/routes/dashboard/arguments.svelte
+++ src/routes/dashboard/arguments.svelte
@@ -9,9 +9,43 @@
 </script>
 
 <script lang="ts">
-	import { setPartyPosition, removePartyPosition, createArgument } from '../../supabase';
+	import { supabase, setPartyPosition, removePartyPosition, createArgument } from '../../supabase';
 	import type { Argument, Arguments, Party } from '../../supabase';
 	export let allArguments: Arguments, parties: Party[];
+	import { onMount } from 'svelte';
+	let loaded: boolean = false;
+	let authenticated: boolean = false;
+	let user = supabase.auth.user();
+
+	const checkIfAuthenticated = async () => {
+		if (user) {
+			const { data, error } = await supabase.from('admin').select('*').eq('email', user.email);
+			if (data!.length > 0) {
+				authenticated = true;
+			} else {
+				authenticated = false;
+			}
+		} else {
+			authenticated = false;
+		}
+	};
+
+	onMount(async () => {
+		await checkIfAuthenticated();
+		loaded = true;
+	});
+
+	const logIn = async (e: SubmitEvent) => {
+		const target = e.target as HTMLFormElement;
+
+		const { user: _user, error } = await supabase.auth.signIn({
+			email: target.email.value,
+			password: target.password.value
+		});
+		if (error) return alert(error.message);
+		user = _user;
+		await checkIfAuthenticated();
+	};
 
 	const moveParty = async (e: Event, argumentID: string, view: string) => {
 		const target = e.target as HTMLSelectElement;
@@ -46,60 +80,77 @@
 	};
 </script>
 
-<div class="dashboard">
-	<div class="create-argument">
-		<h3>create argument</h3>
-		<form on:submit|preventDefault={handleForm}>
-			<input type="text" placeholder="id" name="_id" required />
-			<input type="text" placeholder="name" name="_title" required />
-			<input type="text" placeholder="description" name="description" required />
-			<button type="submit">create</button>
+{#if !loaded}
+	<p>loading</p>
+{:else if !authenticated}
+	<div class="login">
+		<form on:submit|preventDefault={logIn}>
+			<input type="email" name="email" placeholder="email" />
+			<input type="password" name="password" placeholder="password" />
+			<button type="submit">login</button>
 		</form>
 	</div>
-	<hr />
-
-	{#each Object.entries(allArguments) as [id, arg]}
-		<input type="text" name="_id" placeholder="id" value={arg.id} />
-		<br />
-		<input type="text" name="_title" placeholder="title" value={arg.title} />
-		<br />
-		<textarea type="text" name="description" placeholder="description" value={arg.description} />
-		<br>
-		<button on:click={async () => {await deleteArgument(id)}}>delete</button>
-		<h3>d'accordo</h3>
-		{#each Object.entries(arg.parties) as [view, viewParties]}
-			<h3>{view}</h3>
-			<div class="view">
-				<select
-					on:change={async (e) => {
-						await moveParty(e, arg.id, view);
-					}}
-				>
-					<option>add a party</option>
-					{#each parties as party}
-						{#if !viewParties.map((x) => x.initial).includes(party.initial)}
-							<option value={party.initial}>{party.name}</option>
-						{/if}
-					{/each}
-				</select>
-				<br />
-				<div class="parties">
-					{#each viewParties as party}
-						<div class="party">
-							<img src={party.logo} alt="party logo" width="40" />
-							<button
-								on:click={async () => {
-									await removePartyPosition(party.initial, id);
-								}}>remove</button
-							>
-						</div>
-					{/each}
-				</div>
-			</div>
-		{/each}
+{:else}
+<button on:click={async () => {await supabase.auth.signOut();user=null;authenticated=false;}}>signout</button>
+	<div class="dashboard">
+		<div class="create-argument">
+			<h3>create argument</h3>
+			<form on:submit|preventDefault={handleForm}>
+				<input type="text" placeholder="id" name="_id" required />
+				<input type="text" placeholder="name" name="_title" required />
+				<input type="text" placeholder="description" name="description" required />
+				<button type="submit">create</button>
+			</form>
+		</div>
 		<hr />
-	{/each}
-</div>
+
+		{#each Object.entries(allArguments) as [id, arg]}
+			<input type="text" name="_id" placeholder="id" value={arg.id} />
+			<br />
+			<input type="text" name="_title" placeholder="title" value={arg.title} />
+			<br />
+			<textarea type="text" name="description" placeholder="description" value={arg.description} />
+			<br />
+			<button
+				on:click={async () => {
+					await deleteArgument(id);
+				}}>delete</button
+			>
+			<h3>d'accordo</h3>
+			{#each Object.entries(arg.parties) as [view, viewParties]}
+				<h3>{view}</h3>
+				<div class="view">
+					<select
+						on:change={async (e) => {
+							await moveParty(e, arg.id, view);
+						}}
+					>
+						<option>add a party</option>
+						{#each parties as party}
+							{#if !viewParties.map((x) => x.initial).includes(party.initial)}
+								<option value={party.initial}>{party.name}</option>
+							{/if}
+						{/each}
+					</select>
+					<br />
+					<div class="parties">
+						{#each viewParties as party}
+							<div class="party">
+								<img src={party.logo} alt="party logo" width="40" />
+								<button
+									on:click={async () => {
+										await removePartyPosition(party.initial, id);
+									}}>remove</button
+								>
+							</div>
+						{/each}
+					</div>
+				</div>
+			{/each}
+			<hr />
+		{/each}
+	</div>
+{/if}
 
 <style lang="scss">
 	.view {
--- src/supabase.ts
+++ src/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-const supabase = createClient(
+export const supabase = createClient(
 	import.meta.env.VITE_SUPABASE_URL!,
 	import.meta.env.VITE_SUPABASE_KEY!
 );
